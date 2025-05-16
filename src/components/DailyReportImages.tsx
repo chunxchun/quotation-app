8709d65586fb57b17b99b5feda545ffaf3337f2d
William Wu <chun@chunxchun.com>
2025-05-16 10:28:29 +0800

    add remove button

--- src/components/DailyReportImages.tsx
+++ src/components/DailyReportImages.tsx
@@ -6,38 +6,67 @@ import type {
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { DatePicker } from "./DatePicker";
+import { Button } from "./ui/button";
 
 export function DailyReportImages() {
   const [dailyReport, setDailyReport] = useState<Array<DailyReportImage>>();
   const [date, setDate] = useState<Date | undefined>(undefined);
+  const [isFetching, setIsFetching] = useState(false);
 
-  useEffect(() => {
+  const fetchData = async () => {
     if (!date) return;
     const parsedDate = date.toLocaleDateString("en-CA");
     const [yyyy, mm, dd] = parsedDate?.split("-");
 
-    const fetchData = async () => {
-      try {
-        const fetchUrl = `${BASE_API_URL}/daily-reports/images/${yyyy}/${mm}/${dd}`;
-        const response = await fetch(fetchUrl, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${BEARER_TOKEN}`,
-          },
-        });
+    try {
+      const fetchUrl = `${BASE_API_URL}/daily-reports/images/${yyyy}/${mm}/${dd}`;
+      const response = await fetch(fetchUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${BEARER_TOKEN}`,
+        },
+      });
 
-        const { success, message, results } =
-          (await response.json()) as DailyReportImageResponse;
-        if (success) {
-          toast.success("fetch success", {
-            description: message,
-          });
-        }
-        setDailyReport(results);
-      } catch (err) {
-        toast.error(JSON.stringify(err));
+      const { success, message, results } =
+        (await response.json()) as DailyReportImageResponse;
+      if (success) {
+        toast.success("fetch success", {
+          description: message,
+        });
       }
-    };
+      setDailyReport(results);
+    } catch (err) {
+      toast.error(JSON.stringify(err));
+    }
+  };
+
+  const deleteImage = async (url: string) => {
+    console.log("delete", url);
+    setIsFetching(true);
+    try {
+      const fetchUrl = `${BASE_API_URL}/daily-reports/images/${url}`;
+      const response = await fetch(fetchUrl, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${BEARER_TOKEN}`,
+        },
+      });
+
+      const { success, message } =
+        (await response.json()) as DailyReportImageResponse;
+
+      if (success) {
+        toast.success(`success delete image`, { description: message });
+      }
+      fetchData();
+      setIsFetching(false);
+    } catch (err) {
+      toast.error(JSON.stringify(err));
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, [date]);
 
@@ -51,13 +80,22 @@ export function DailyReportImages() {
           return (
             <div key={daily.id} className="flex flex-col py-4">
               <p className="w-full text-wrap">{daily.desc}</p>
-              <p className="w-full text-wrap">{`${daily.building}${daily.level}${daily.location}${daily.substrate}${daily.work}`}</p>
-              {/* <div className="w-full p-2 aspect-square border"> */}
+              <p className="w-full text-wrap">{`${daily.building}${daily.level} ${daily.location}${daily.substrate}${daily.work}`}</p>
+
               <img
                 className=" aspect-square object-cover"
                 src={`${R2_URL}/${daily.url}`}
                 alt={daily.desc}
               />
+
+              <Button
+                variant="destructive"
+                className="m-4 w-1/2 self-center"
+                onClick={() => deleteImage(encodeURIComponent(daily.url))}
+                disabled={isFetching}
+              >
+                Delete
+              </Button>
               {/* </div> */}
               {/* <img src={`${BASE_API_URL}/api/${daily.url}`} alt={daily.desc} /> */}
             </div>
